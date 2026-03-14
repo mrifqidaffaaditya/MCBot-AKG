808c35e11975295e9f61e0fa7599f54d1ece4dd6
mrifqidaffaaditya <mrifqidaffaaditya10@gmail.com>
2026-03-14 11:36:22 +0700

    aaa

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    "mineflayer",
+    "minecraft-protocol",
+    "minecraft-data",
+    "prismarine-physics",
+    "prismarine-world",
+    "prismarine-chunk",
+    "prismarine-block",
+    "prismarine-entity",
+    "prismarine-item",
+    "prismarine-windows",
+    "prismarine-recipe",
+    "prismarine-biome",
+    "prismarine-nbt",
+    "prismarine-chat",
+    "minecraft-assets",
+  ],
 };
 
 export default nextConfig;
